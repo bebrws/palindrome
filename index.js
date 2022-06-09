8998bcc8c7a1de46ca54437b107de984fb3c2884
Brad Barrows <bradley.barrows@pluto.tv>
2022-06-09 15:00:01 -0700

    Initial commit for the palindrome problem

---
+++ index.js
@@ -0,0 +1,28 @@
+// Please implement the function isPalindrome to pass the test cases below.
+function isPalindrome(inputString) {
+    throw new Error("Function not implemented.");
+}
+
+function testCase(description, inputString, expectedResult) {
+    const result = isPalindrome(inputString);
+    let resultString;
+    if (result === expectedResult) {
+        resultString = "✅ Passed";
+    } else {
+        resultString = "❌ Failed"
+    }
+    console.log(`${resultString} ${description}: '${inputString}'`)
+}
+
+testCase("One letter", "a", true);
+testCase("Two different letters", "an", false);
+testCase("Duplicate letters", "aa", true);
+testCase("Three letter palindrome", "ana", true);
+testCase("Four letter palindrome", "noon", true);
+testCase("Four letter non-palindrome", "nine", false);
+testCase("Seven letter palindrome", "racecar", true);
+testCase("Ignores case", "Racecar", true);
+testCase("Ignores odd spaces", "taco cat", true);
+testCase("Ignores non-letters", "racecar?", true);
+testCase("Large palindrome ignores case, spaces and punctation", "Eva, can I see bees in a cave?", true);
+testCase("Large non-palindrome with case, spaces and punctation", "Did you hear that?", false);
